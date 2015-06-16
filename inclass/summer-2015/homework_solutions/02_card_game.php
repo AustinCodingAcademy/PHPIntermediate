020b4521c0848253dbc1b58e9eefeeeefebfd403
Samir Patel <samir.patel@wholefoods.com>
2015-06-16 18:58:56 -0500

    Solution for Card game: FULL

--- inclass/summer-2015/homework_solutions/02_card_game.php
+++ inclass/summer-2015/homework_solutions/02_card_game.php
@@ -1,62 +1,112 @@
 <?php
-//<div style="font-size: 5em; color:red;">&diam;</div>
-//<div style="font-size: 5em; color:red;">&spades;</div>
-//<div style="font-size: 5em; color:black;">&clubs;</div>
-//<div style="font-size: 5em; color:red;">&hearts;</div>
 
 /**
- * This will return a deck of cards
+ * Return an array that represents a deck of cards
+ *
+ * e.g. array(
+ *  0 => '1D', // Ace of diamonds
+ *  1 => '2D', // 2 of diamonds
+ *  ...
+ *  11 => '11C' // Jack of clubs
+ * )
+ *
  * @return array
  */
 function getDeck()
 {
-    /** @var array $deck My Deck of cards */
-    $deck = [];
+    $suites = array('D' => '&diams;', 'H' => '&hearts;', 'S' => '&spades;', 'C' => '&clubs;');
+    $ranks = array_merge(range(2, 10), array('J', 'Q', 'K', 'A'));
 
-    $suites = [
-        'Diamond' => '&diams;',
-        'Heart' => '&hearts;',
-        'Club' => '&clubs;',
-        'Spade' => '&spades;'
-    ];
-    $ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-
-    // Loop through all the suites
-    foreach ($suites as $suite => $color) {
+    $deck = array();
+    foreach ($suites as $suite => $suiteColor) {
 
         foreach ($ranks as $rank) {
 
-            // Create a card?
-//            $deck[] = $suite . '-' . $rank;
+            // Diamonds and hearts are red
+            if ($suite == 'D' || $suite == 'H') {
+                $color = 'red';
+            } else { // Spades and clubs are black
+                $color = 'black';
+            }
 
-            // Using the function
-            $deck[] = colorizeCard($suite, $color, $rank);
+            $deck[] = '<span style="color: ' . $color . ';">' . $rank . '' . $suiteColor . '</span>';
         }
     }
-
-    // Make it happen :)
-    // Hint, use two nested loops.
-
     return $deck;
 }
 
-
 /**
- * Colorize the card
- * @param string $suite English name of the suite
- * @param string $htmlSuite HTML Entity icon
- * @param string $rank The numeric or alpha-numeric rank
- * @return string
+ * Shuffle the deck of cards
+ *
+ * @param array $deck Full deck of cards (passed by ref)
+ *
+ * @return void
  */
-function colorizeCard($suite, $htmlSuite, $rank)
+function shuffleDeck(&$deck)
 {
-    $color = ($suite == 'Diamond' || $suite == 'Heart') ? 'red' : 'black';
-
-    return "<div style=\"font-size: 5em; color:$color;\">$htmlSuite$rank</div>";
+    shuffle($deck);
 }
 
-// Using the deck function
-$myDeck = getDeck();
-echo 'Number of cards in deck: ' . count($myDeck) . '<br/>';
+/**
+ * @param array $players      An array of player names
+ * @param int   $numCards     How many cards to give each player
+ * @param array $shuffledDeck A shuffled deck of cards
+ *
+ * @return array
+ */
+function deal($players, $numCards, &$shuffledDeck)
+{
+    /** @var array $playersHands This is the array we will construct and return that has the hands we deal */
+    $playersHands = array();
+
+    foreach($players as $player){
+
+        /** @var array $playerCards Of cards that each player will get */
+        $playerCards = array();
+
+        foreach($shuffledDeck as $key => $card){
+
+            // Give a card to the player
+            $playerCards[] = $card;
+
+            // Remove the given card from the deck
+            unset($shuffledDeck[$key]);
+
+            // If we have given the player the number of cards they need
+            // break out of the loop
+            if(sizeof($playerCards) == $numCards){
+                break;
+            }
+        }
+
+        // Assign the hand to the player
+        $playersHands[$player] = $playerCards;
+    }
+
+    return $playersHands;
+}
+
+
+// ----------- USAGE -----------------
+
 echo '<pre>';
-print_r($myDeck);
+// Crack open a brand new deck of cards
+$deck = getDeck();
+
+// Shuffle the deck
+shuffleDeck($deck);
+
+echo 'Deck after shuffling, but before dealing: <br/>';
+print_r($deck);
+
+$players = array('Joe', 'Mary', 'Zim', 'Samir', 'Ryan');
+$numCards = 4;
+
+$playerHands = deal($players, $numCards, $deck);
+
+echo 'Hands each player has: <br/>';
+print_r($playerHands);
+
+echo 'Deck has '.sizeof($deck).' cards after dealing: <br/>';
+print_r($deck);
+echo '<pre>';
