34aeed8443594148921fb9cc0f6c5b4ba4329c21
Samir Patel <samir.patel@wholefoods.com>
2015-09-17 19:17:57 -0500

    Yayy

---
+++ inclass/fall-2015/module-1/arrays1.php
@@ -0,0 +1,22 @@
+<?php
+
+$pets = ['dog', 'cat', 'beast'];
+$flowers = ['rose', 'sunflower', 'mayflower'];
+
+// TIMEBOX: 5 minutes
+// 1. Merge both these arrays together
+$merged = array_merge($pets, $flowers);
+echo '<pre>';
+print_r($merged);
+
+// 2. Sort the arrays alphabetically, once merged
+sort($merged);
+
+echo '<h3>Sorted</h3>';
+echo '<pre>';
+print_r($merged);
+
+// 3. Count the number of elements in the array
+echo 'The array has ' . count($merged) . ' elements' . PHP_EOL;
+
+// (optional) 4. Create a comma separated string from the arrays
